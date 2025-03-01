230c40c87305d77ccb44e9e40977ff13c749af94
Santitub <126692870+Santitub@users.noreply.github.com>
2025-03-01 23:48:22 +0100

    Delete scripts directory

--- scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user_enumeration import check_user_enumeration
-from .xmlrpc_analyzer import check_xmlrpc
-from .sensitive_files import scan_sensitive_files
-from .wp_version import detect_wp_version
-from .rest_api_analyzer import check_rest_api
